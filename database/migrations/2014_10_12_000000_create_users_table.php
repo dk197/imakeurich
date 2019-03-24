b2e3ea3c2a90ab0821669daa40ad0406fdfbfa3d
dk_laptop <dominik_schaefer97@yahoo.com>
2019-03-24 07:43:32 +0100

    basic user adjustments

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('username');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->float('balance', 9, 2)->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
