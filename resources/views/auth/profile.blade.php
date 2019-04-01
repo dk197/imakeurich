5436b4fb2b4607425127de339029f9575396526e
liltschou <38752135+liltschou@users.noreply.github.com>
2019-04-01 16:52:09 +0200

    Added custom form of address and function displaying the time since first login

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.app')
 
+<?php
+//Wandelt den created_at Timestamp in eine dynamische Anzeige um, wie lange man schon online ist.
+function time_elapsed_string($datetime, $full = false) {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'Jahr/e',
+        'm' => 'Monat/e',
+        'w' => 'Woche/n',
+        'd' => 'Tag/en',
+        'h' => 'Stunde/n',
+        'i' => 'Minute/n',
+        's' => 'Sekunde/n',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? '' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . '' : '';
+}
+$timeago = auth()->user()->created_at;
+?>
+
 @section('content')
-<p> hallo </p>
+<div class="container">
+    <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
+      <div class="col-md-6 px-0">
+      <h1 class="display-4 font-italic">Hello {{Auth::user()->username}},</h1>
+        <p class="lead my-3">du zockst jetzt schon seit <?php echo time_elapsed_string($timeago); ?> und das sind deine Statistiken:</p>
+      </div>
+    </div>
+
+
 @endsection
