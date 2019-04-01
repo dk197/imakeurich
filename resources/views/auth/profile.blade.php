64ed704693eebb312358c6f4780935a67deba932
liltschou <38752135+liltschou@users.noreply.github.com>
2019-04-01 16:54:58 +0200

    Changed Wording

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -37,7 +37,7 @@ $timeago = auth()->user()->created_at;
 <div class="container">
     <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
       <div class="col-md-6 px-0">
-      <h1 class="display-4 font-italic">Hello {{Auth::user()->username}},</h1>
+      <h1 class="display-4 font-italic">Hallo {{Auth::user()->username}},</h1>
         <p class="lead my-3">du zockst jetzt schon seit <?php echo time_elapsed_string($timeago); ?> und das sind deine Statistiken:</p>
       </div>
     </div>
