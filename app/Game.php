df5307f13c979220e48ba6d4e80845f6857017af
liltschou <38752135+liltschou@users.noreply.github.com>
2019-04-01 11:56:29 +0200

    Revert "Merge branch 'master' of https://github.com/dk197/imakeurich"
    This reverts commit 50fed8c3e6fd03e1a775f8d8b4280777dfda5147, reversing
    changes made to fedd4633ea47057c00ccfb56c32a8a174c4fc563.

--- app/Game.php
+++ app/Game.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use App\Game;
 use App\Player;
-use Pusher\Pusher;
 
 class Game extends Model
 {
@@ -15,102 +14,25 @@ class Game extends Model
     ];
 
     public function player(){
-    	return $this->hasMany(Player::class)->orderBy('bid', 'DESC');
+    	return $this->hasMany(Player::class)->orderBy('bid', 'DESC');;
     }
 
     public function addPlayer($bid){
 
     	$user = auth()->user();
 
-    	$result = Player::create([
+    	return Player::create([
     		'user_id' => $user->id,
     		'username' => $user->username,
     		'game_id' => $this->id,
     		'bid' => $bid
     	]);
-
-        //the position of the player in the game
-        $position = $this->getPlayerPosition($this->id, $result->id) + 1;
-
-        
-        // ################# Pusher start #############################
-
-            $options = array(
-                'cluster' => 'eu'
-            );
-     
-            $pusher = new Pusher(
-                env('PUSHER_APP_KEY'),
-                env('PUSHER_APP_SECRET'),
-                env('PUSHER_APP_ID'),
-                $options
-            );
-
-            $data = ([
-                'game_id' => $this->id,
-                'bid' => $bid,
-                'user_id' => $user->id,
-                'username' => $user->username,
-                'position' => $position
-            ]);
-            
-            $pusher->trigger('player_enter', 'player_enter-event', $data);  
-
-        // ################# Pusher end ###############################
-
     }
 
     public function updatePlayerBid($bid, $game){
         
         $user = auth()->user();
-
-        //get Entry which will get updated
-        $result = Player::where(['user_id' => $user->id, 'game_id' => $game->id]);
-
-        // update the Entry
-        $result->update(['bid' => DB::raw('bid +'.$bid)]);
-
-        // convert the Entry-Object to an array
-        $result_array = $result->get()->toArray();
-
-        //the position of the player in the game
-        $position = $this->getPlayerPosition($game->id, $result_array[0]['id']) + 1;
-
-
-        // ################# Pusher start #############################
-
-            $options = array(
-                'cluster' => 'eu'
-            );
-     
-            $pusher = new Pusher(
-                env('PUSHER_APP_KEY'),
-                env('PUSHER_APP_SECRET'),
-                env('PUSHER_APP_ID'),
-                $options
-            );
-
-            $data = ([
-                'game_id' => $game->id,
-                'bid' => $bid,
-                'user_id' => $user->id,
-                'username' => $user->username,
-                'position' => $position
-            ]);
-            
-            $pusher->trigger('player_update', 'player_update-event', $data);  
-
-        // ################# Pusher end ###############################
-
-    }
-
-    //get the Position of the Player based on his bid
-    public function getPlayerPosition($game_id, $player_id){
-        $all_players = DB::table('players')->where(['game_id' => $game_id])->orderBy('bid', 'DESC')->get()->toArray();
-
-        //search for the player ID in the Array of all Player of the selected game and get the index (that array is descending ordered by the Bid)
-        $key = array_search($player_id, array_column($all_players, 'id'));
-
-        return $key;
+        
+        Player::where(['user_id' => $user->id, 'game_id' => $game->id])->update(['bid' => DB::raw('bid +'.$bid)]);
     }
 }
