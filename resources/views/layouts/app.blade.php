17a591cb0f56707184c2ec296eb0b7084594889a
liltschou <38752135+liltschou@users.noreply.github.com>
2019-04-01 20:50:31 +0200

    Added Profilelink to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -65,6 +65,7 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="/user/{{Auth::user()->id}}">Your Profile</a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
