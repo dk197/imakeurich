2c3cde6b5fa79e05fd0fa6c379939024ff0ada55
liltschou <38752135+liltschou@users.noreply.github.com>
2019-04-01 17:08:34 +0200

    Update profile.blade.php

--- resources/views/auth/profile.blade.php
+++ resources/views/auth/profile.blade.php
@@ -38,9 +38,22 @@ $timeago = auth()->user()->created_at;
     <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
       <div class="col-md-6 px-0">
       <h1 class="display-4 font-italic">Hallo {{Auth::user()->username}},</h1>
-        <p class="lead my-3">du zockst jetzt schon seit <?php echo time_elapsed_string($timeago); ?> und das sind deine Statistiken:</p>
+        <p class="lead my-3">hier kannst du die wichtigsten deiner persönlichen Statistiken:</p>
       </div>
     </div>
 
-
+    <div class="row mb-2">
+      <div class="col-md-6">
+        <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+          <div class="col p-4 d-flex flex-column position-static">
+            <strong class="d-inline-block mb-2 text-primary">Warum bin ich nicht so damn rich wie du?</strong>
+            <h3 class="mb-0">Du bist rich seit</h3>
+            <p class="card-text mb-auto">(erste Anmeldung)</p>
+          </div>
+          <div class="jumbotron col-auto d-none d-lg-block">
+            <h1><?php echo time_elapsed_string($timeago); ?></h1>
+          </div>
+        </div>
+      </div>
+    </div>
 @endsection
