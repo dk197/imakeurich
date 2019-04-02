8fe8ebeec6f456d5dd655220e25a624ca40175ba
liltschou <38752135+liltschou@users.noreply.github.com>
2019-04-02 16:51:57 +0200

    update UserProfile

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -34,47 +34,60 @@ $timeago = $user->created_at;
 ?>
 
 @section('content')
-<div class="container">
-    <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-      <div class="col-md-6 px-0">
-      <h1 class="display-4 font-italic">Thats {{$user->username}},</h1>
-        <p class="lead my-3">here can you see all his personal statistics:</p>
-      </div>
-    </div>
 </div>
 
-
-<div class="counter">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="employees">
-                    <p class="counter-count1"><?php echo time_elapsed_string($timeago); ?></p>
-                    <p class="employee-p">since he got rich</p>
+<div id="banner">
+    <div id="cloud-scroll">
+        <div class="container mt-3">
+            <div class="jumbotron p-4 p-md-5 text-dark rounded bg-transparent">
+                <div class="row">
+                    <div class="col-md-6 px-0">
+                        <h1 class="display-4 font-italic">Thats {{$user->username}},</h1>
+                        <p class="lead my-3">here can you see all his personal statistics:</p>
+                    </div>
+                    <div class="col-md-6 px-0">
+                        <!--button-->
+                        
+                    </div>
                 </div>
             </div>
+        </div>
+        <div class="container">
+        <div class="counter">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                            <div class="employees">
+                                <p class="counter-count1"><?php echo time_elapsed_string($timeago); ?></p>
+                                <p class="employee-p">since he got rich</p>
+                            </div>
+                        </div>
 
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="customer">
-                    <p class="counter-count"><?php echo $countbids ?></p>
-                    <p class="customer-p">bids were taken</p>
-                </div>
-            </div>
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                            <div class="customer">
+                                <p class="counter-count"><?php echo $countbids ?></p>
+                                <p class="customer-p">bids were taken</p>
+                            </div>
+                        </div>
 
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="design">
-                    <p class="counter-count"><?php echo $coinsbid ?></p>
-                    <p class="design-p">coins were bid</p>
-                </div>
-            </div>
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                            <div class="design">
+                                <p class="counter-count"><?php echo $coinsbid ?></p>
+                                <p class="design-p">coins were bid</p>
+                            </div>
+                        </div>
 
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="order">
-                    <p class="counter-count"><?php echo $coinswon ?></p>
-                    <p class="order-p">coins were won</p>
+                        <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                            <div class="order">
+                                <p class="counter-count"><?php echo $coinswon ?></p>
+                                <p class="order-p">coins were won</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<div>
 @endsection
