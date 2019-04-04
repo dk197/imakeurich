950cb9623563567523e1490f3436491b09496d07
navewi <nathalie.wich@web.de>
2019-04-04 10:59:09 +0200

    removed home blade and changed routes after login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,19 +65,6 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            <!-- @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif -->
             <div class="content">
                 <div class="title m-b-md">
                     I make u rich
@@ -88,7 +75,7 @@
                     @if (Route::has('login'))
 
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/games') }}">Games</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
