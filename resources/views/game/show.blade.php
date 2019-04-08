553ec1a6f55b32c165ae31df63f53f6d372bd5e5
monamuhr <wolf.nathalie@gmx.de>
2019-04-08 11:16:12 +0200

    minor text changes

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -6,10 +6,10 @@
 		<h1>Multi Bid Millionaire</h1>
 	</div>
 	<div class="col-4">
-		<p id="player_number">Spieler: {{ $player_number }}/{{ $game->max_players }}</p>
+		<p id="player_number">Player: {{ $player_number }}/{{ $game->max_players }}</p>
 	</div>
 	<div class="col-4 text-center">
-		<p>Minimal bid: {{ $game->min_bid }} IGW | Pot limit: {{ $game->igw_limit }} IGW</p>
+		<p>Minimal bid: {{ $game->min_bid }} Coins | Pot limit: {{ $game->igw_limit }} IGW</p>
 	</div>
 	<div class="col-4 text-right">
 		<p>Winning Places: 1, {{ $game->win_1 }}, {{ $game->win_2 }}, {{ $game->win_3 }}</p>
@@ -21,8 +21,8 @@
 		<form method="POST" action="/games/{{ $game->id }}/enter" id="player_enter_form">
 			@csrf
 			<div class="form-group">
-				<label for="game_bid">How much money do you wanna drop in?</label>
-				<input type="number" name="game_bid" id="game_bid" class="form-control" placeholder="Amount of IGW u wanna spend">
+				<label for="game_bid">How much coins do you wanna drop in?</label>
+				<input type="number" name="game_bid" id="game_bid" class="form-control" placeholder="Amount of coins u wanna spend">
 			</div>
 			<button type="submit" class="btn button-gold enter-button">GO IN</button>
 		</form>
@@ -42,7 +42,7 @@
 					<tr>
 						<th class="col_1" scope="row" style="width: 33%">{{ $loop->iteration }}.</th>
 						<td class="text-center col_2" style="width: 33%">{{ $player->username }} ({{ $player->bid }})</td>
-                    <td class="text-right col_3" style="width: 33%"><a href="/user/{{$player->id}}">Zum Profil</a></td>
+                    <td class="text-right col_3" style="width: 33%"><a href="/user/{{$player->id}}">Show profile</a></td>
 					</tr>
 				@endforeach
 			</tbody>
