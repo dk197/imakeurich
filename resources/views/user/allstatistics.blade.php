e64f60ccbfc96f7e60666e1e2415414157847df6
monamuhr <wolf.nathalie@gmx.de>
2019-04-08 10:35:56 +0200

    allstatistics css changes

--- resources/views/user/allstatistics.blade.php
+++ resources/views/user/allstatistics.blade.php
@@ -2,46 +2,46 @@
 
 @section('content')
 <div class="container">
-    <div class="jumbotron p-4 p-md-5 text-white rounded statistics-card">
-      <div class="col-md-6 px-0">
       <h1 class="display-4">All-Time Statistics</h1>
-        <p class="lead my-3">Here can you see all statistics:</p>
-      </div>
+
+
+<div class="row mt-4">
+<div class="card col-sm-12">
+    <div class="card-header text-center">
+    overview of all games that have ever been played
     </div>
-</div>
+    <div class="card-body">
+        <div class="counter">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                        <div class="employees">
+                            <p class="counter-count"><?php echo $gamesplayed ?></p>
+                            <p class="employee-p">games were played</p>
+                        </div>
+                    </div>
 
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                        <div class="customer">
+                            <p class="counter-count"><?php echo $countbids ?></p>
+                            <p class="customer-p">bids were taken</p>
+                        </div>
+                    </div>
 
-<div class="counter">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="employees">
-                    <p class="counter-count"><?php echo $gamesplayed ?></p>
-                    <p class="employee-p">games were played</p>
-                </div>
-            </div>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                        <div class="design">
+                            <p class="counter-count"><?php echo $coinsbid ?></p>
+                            <p class="design-p">coins were bid</p>
+                        </div>
+                    </div>
 
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="customer">
-                    <p class="counter-count"><?php echo $countbids ?></p>
-                    <p class="customer-p">bids were taken</p>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="design">
-                    <p class="counter-count"><?php echo $coinsbid ?></p>
-                    <p class="design-p">coins were bid</p>
-                </div>
-            </div>
-
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <div class="order">
-                    <p class="counter-count"><?php echo $coinswon ?></p>
-                    <p class="order-p">coins were won</p>
+                    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                        <div class="order">
+                            <p class="counter-count"><?php echo $coinswon ?></p>
+                            <p class="order-p">coins were won</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
 @endsection
